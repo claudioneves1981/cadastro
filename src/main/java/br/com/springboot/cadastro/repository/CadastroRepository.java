473d1828d17cd6b004a9dc847a34e5c83b58f701
Claudio <cfneguacu@uni9.edu.br>
2021-06-18 18:35:20 -0300

    atualização

--- src/main/java/br/com/springboot/cadastro/repository/CadastroRepository.java
+++ src/main/java/br/com/springboot/cadastro/repository/CadastroRepository.java
@@ -9,6 +9,17 @@ import br.com.springboot.cadastro.model.Cadastro;
 @Repository
 public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
 
-	@Query(value = "select c from Cadastro c where upper(trim(c.nome)) like %?1%")
+	@Query(value = "select c from Cadastro c where upper(trim(c.nome)) like %?1% or "+
+												  "upper(trim(c.idade)) like %?1% or "+
+												  "upper(trim(c.endereco)) like %?1% or "+
+												  "upper(trim(c.numero)) like %?1% or "+
+												  "upper(trim(c.bairro)) like %?1% or "+
+												  "upper(trim(c.cep)) like %?1% or "+
+												  "upper(trim(c.cidade)) like %?1% or "+
+												  "upper(trim(c.estado)) like %?1% or "+
+												  "upper(trim(c.telefone1)) like %?1% or "+
+												  "upper(trim(c.telefone2)) like %?1% or "+
+												  "upper(trim(c.quantosmoram)) like %?1% or "+
+												  "upper(trim(c.numeronis)) like %?1%")
 	List<Cadastro> buscarPorNome(String name);
 }
